0ab202d775ba2c39bd4f6bb447fac51802bfc86c
gutsy47 <bestcom5055@gmail.com>
2022-11-19 02:52:27 +0300

    Feat: Snake fill
    snakeFill and additional functions have been created. Dynamic matrix update print need to be upgraded cuz system("cls") not only a bad idea, but moreover it's hurts the eyes.
    Perhaps there's a better way to do this fill method, looking for it rn.

--- main.cpp
+++ main.cpp
@@ -1,3 +1,8 @@
+#include <iostream>
+#include <cstdlib>
+#include <chrono>
+#include <thread>
+
 // 1) Используя арифметику указателей, заполняет квадратичную целочисленную матрицу порядка N (6,8,10)
 //    случайными числами от 1 до N*N согласно схемам, приведенным на рисунках (спираль и змейка).
 //    Пользователь должен видеть процесс заполнения квадратичной матрицы.
@@ -5,7 +10,47 @@
 // 3) Используя арифметику указателей, сортирует элементы любой сортировкой.
 // 4) Уменьшает, увеличивает, умножает или делит все элементы матрицы на введенное пользователем число.
 
+void printMatrix(int *arr, int N) {
+    for (int i = 0; i < N*N; i+=N) {
+        for (int j = i; j < i+N; j++) {
+            std::cout << arr[j] << '\t';
+        }
+        putchar('\n');
+    }
+}
+
+int getRandInt(int from, int to) {
+    return -from + std::rand() % (from + to + 1);
+}
+
+void snakeFill(int *arr, int N) {
+    int *pArr = arr;
+    int change = N;
+    for (int i = 0; i < N; i++) {
+        int *pRowEnd = (change > 0) ? pArr + N*N - N : pArr - N*N + N;
+        while (pArr - change != pRowEnd) {
+            *pArr = getRandInt(1, 100);
+
+            system("cls");
+            printMatrix(arr, N);
+            std::this_thread::sleep_for(std::chrono::milliseconds(500));
+
+            pArr += change;
+        }
+        pArr -= change - 1;
+        change = -change;
+    }
+}
+
 int main() {
+    const int N = 6;
+    int array[N*N];
+
+    for (int i = 0; i < N*N; i++) {
+        array[i] = 0;
+    }
+
+    snakeFill(array, N);
 
     return 0;
 }
