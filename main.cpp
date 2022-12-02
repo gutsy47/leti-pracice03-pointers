ebdffcb09122557b3bdd546b83e127ad2b4a94fc
gutsy47 <bestcom5055@gmail.com>
2022-12-02 17:46:58 +0300

    Feat: Menu with infinite loop
    All the UI/UX has been provided.
    Also wrote the changeMatrixValue() function for task 4.
    
    Signed-off-by: gutsy47 <bestcom5055@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 #include <cstdlib>
 #include <Windows.h>
 
@@ -123,9 +124,32 @@ void swapQuadrants(int *arr, int N, int q1, int q2) {
 }
 
 void insertionSort(int *arr, int N) {
-    for (int *p1 = arr+1; p1 < arr + N; ++p1)
+    for (int *p1 = arr+1; p1 < arr + N*N; ++p1)
         for (int *p2 = p1; p2 > arr && *(p2-1) > *p2; --p2)
             std::swap(*(p2-1), *p2);
+}
+
+void changeMatrixValues(int *arr, int N, int operation, int diff) {
+    for (int *pArr = arr; pArr < arr + N*N; ++pArr) {
+        switch (operation) {
+            case '+': {
+                *pArr += diff;
+                break;
+            }
+            case '-': {
+                *pArr -= diff;
+                break;
+            }
+            case '*': {
+                *pArr *= diff;
+                break;
+            }
+            case '/': {
+                *pArr /= diff;
+                break;
+            }
+        }
+    }
 }
 
 int main() {
@@ -134,6 +158,148 @@ int main() {
     int matrix[N][N];
     int *ptrMatrix = &matrix[0][0];
     zeroFill(ptrMatrix, N);
+
+    // Main loop
+    std::cout << "Enter 'h' to get list of commands\n";
+    while (true) {
+
+        // Get command from user
+        char userAction;
+        std::cout << "<< Action: \n>> ";
+        std::cin.sync();
+        std::cin >> userAction;
+
+        // Exit
+        if (userAction == '0') break;
+
+        switch (userAction) {
+
+            // Fill array using snake pattern
+            case '1': {
+                std::cout << "Filling array using snake pattern...\n";
+                snakeFill(ptrMatrix, N);
+                std::cout << "Done\n";
+                break;
+            }
+
+            // Fill array using spiral pattern
+            case '2': {
+                std::cout << "Filling array using spiral pattern...\n";
+                spiralFill(ptrMatrix, N);
+                std::cout << "Done\n";
+                break;
+            }
+
+            // Swap blocks
+            case '3': {
+                // User input
+                char userInput;
+                std::cout << "<< Select block exchange pattern (a || b || c || d):\n>> ";
+                std::cin >> userInput;
+                std::cin.sync();
+                if (userInput < 97 || userInput > 100) {
+                    std::cout << "Input incorrect. May be another time...\n";
+                    break;
+                }
+
+
+                // Swap
+                std::cout << "Matrix before the swap:\n";
+                printMatrix(ptrMatrix, N);
+                switch (userInput) {
+                    case 'a': {
+                        swapQuadrants(ptrMatrix, N, 1, 2);
+                        swapQuadrants(ptrMatrix, N, 2, 4);
+                        swapQuadrants(ptrMatrix, N, 4, 3);
+                        swapQuadrants(ptrMatrix, N, 3, 1);
+                        break;
+                    }
+                    case 'b': {
+                        swapQuadrants(ptrMatrix, N, 1, 4);
+                        swapQuadrants(ptrMatrix, N, 2, 3);
+                        break;
+                    }
+                    case 'c': {
+                        swapQuadrants(ptrMatrix, N, 1, 3);
+                        swapQuadrants(ptrMatrix, N, 2, 4);
+                        break;
+                    }
+                    case 'd': {
+                        swapQuadrants(ptrMatrix, N, 1, 2);
+                        swapQuadrants(ptrMatrix, N, 3, 4);
+                        break;
+                    }
+                }
+                std::cout << "Matrix after the swap:\n";
+                printMatrix(ptrMatrix, N);
+
+                break;
+            }
+
+            // Sort the matrix
+            case '4': {
+                std::cout << "Matrix before the sorting:\n";
+                printMatrix(ptrMatrix, N);
+                insertionSort(ptrMatrix, N);
+                std::cout << "Sorted using insert sort. Result:\n";
+                printMatrix(ptrMatrix, N);
+                break;
+            }
+
+            // Change all the matrix elements with number entered by user
+            case '5': {
+                // User input
+                int userNumb;
+                std::cout << "<< Enter an integer:\n>> ";
+                std::cin >> userNumb;
+                std::cin.sync();
+                if (std::cin.fail()) {
+                    std::cout << "TypeError: invalid literal for int with base 10.\n";
+                    std::cin.clear();
+                    break;
+                }
+                char userOp;
+                std::cout << "<< Select operation (+ || - || * || /):\n>> ";
+                std::cin >> userOp;
+                std::cin.sync();
+                if (!(userOp == '+' || userOp == '-' || userOp == '*' || userOp == '/')) {
+                    std::cout << "Input incorrect.\n";
+                    break;
+                }
+
+                // Change
+                std::cout << "Matrix before the changes:\n";
+                printMatrix(ptrMatrix, N);
+                changeMatrixValues(ptrMatrix, N, userOp, userNumb);
+                std::cout << "Matrix after:\n";
+                printMatrix(ptrMatrix, N);
+                break;
+            }
+
+            // Help
+            case 'h': {
+                std::cout << "Available commands:\n";
+                std::cout << std::setw(32) << std::setfill('-') << '\n';
+                std::cout << "h: Help\n";
+                std::cout << std::setw(32) << std::setfill('-') << '\n';
+                std::cout << "1: Fill array with random numbers using snake pattern\n";
+                std::cout << "2: Fill array with random numbers using spiral pattern\n";
+                std::cout << "3: Swap blocks of the matrix using patterns from the task\n";
+                std::cout << "4: Sort the matrix using insert sort\n";
+                std::cout << "5: Change (e.g. multiply, divide, etc) all the matrix elements\n";
+                std::cout << std::setw(32) << std::setfill('-') << '\n';
+                std::cout << "0: Exit\n";
+                std::cout << std::setw(32) << std::setfill('-') << '\n';
+                std::cout << "NOTE: Script will skip all the useless input (e.g. extra symbols in action input)\n";
+                std::cout << std::setw(32) << std::setfill('-') << '\n';
+                std::cout << std::setfill(' ');
+                break;
+            }
+
+            // Unknown command error
+            default: std::cout << "RuntimeError: unknown command\n";
+        }
+    }
 
     return 0;
 }
